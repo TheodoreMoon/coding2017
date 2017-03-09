41fa10b0b2c057927d3e8c35e63f7628bd3a3020
zhukun <zhukun@zhukun-PC>
2017-03-09 22:26:08 +0800

    Just Test

---
+++ group27/44555640/src/com/zhku/task1/TestList.java
@@ -0,0 +1,14 @@
+package com.zhku.task1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestList {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
